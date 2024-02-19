a933b55a8761371b07d45f3dd38ce71b353c01c5
YuuuuSeong-Kim <kys980611@gmail.com>
2024-02-19 09:13:39 +0900

    function exports test

---
+++ hello.js
@@ -0,0 +1,4 @@
+var sist = require("./sist")
+
+console.log(sist.add(100));
+console.log(sist.sub(10));
---
+++ sist.js
@@ -0,0 +1,9 @@
+function add(n){
+    return n+1;
+}
+
+function sub(n){
+    return n-1;
+}
+
+module.exports= {add,sub};
